474ce61102434a940164491f711a4c2fd6cab58f
G.M.K.Prasad <prasadchoco@gmail.com>
2020-03-01 00:26:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,49 @@ app.get('/api/customers', (req, res) => {
 });
 
 
+//Display's the List of Customers by customer id
+app.get('/api/customers/:id', (req, res) => {
+    const customer = customers.find(cust => cust.id == parseInt(req.params.id));
+
+    //checks whether customer data existed or not
+    if(!customer){
+        res.status(404).send('<h2>Sorry Customer Data not Found !</h2>');
+    }else{
+        res.send(customer);
+    }
+});
+
+
+//Display's the List of Customers by customer id
+app.post('/api/customers', (req, res) => {
+    const { err } = validateCustData(req.body);
+
+    if(err){
+        res.status(400).send(err.details[0].message);
+        return;
+    }
+    
+    //Increment Customer Id
+    const customer = {
+        title: req.body.title,
+        id: customers.length + 1
+    };
+
+    customers.push(customer);
+    res.send(customer);
+});
+
+function validateCustData(customer) {
+
+    const CustName = {
+        title: joi.string().min(3).required()
+
+    };
+
+    return joi.validate(customer, CustName);
+}
+
+
 //Port Environment Variable
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log('Server started ${port}..'));
